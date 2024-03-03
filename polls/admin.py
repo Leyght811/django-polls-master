73db45a5413c7e7d9e276d917330cc0e7d4c77d0
Leyght811 <wesleyhamish@hotmail.com>
2024-03-04 12:30:06 +1300

    Changed Polls app to resemble the one I made during the django tutorial to avoid deprecations

--- polls/admin.py
+++ polls/admin.py
@@ -1,24 +1,22 @@
-#-*- coding: utf-8 -*-
 from django.contrib import admin
 
-from .models import Choice, Poll
+from .models import Choice, Question
 
 
 class ChoiceInline(admin.TabularInline):
     model = Choice
-    readonly_fields = ('votes',)
     extra = 3
 
 
-class PollAdmin(admin.ModelAdmin):
+class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
-        (None, {
-            'fields': ['question'],
-        }),
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
     ]
     inlines = [ChoiceInline]
-    list_display = ('question',)
-    search_fields = ['question']
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
 
-admin.site.register(Poll, PollAdmin)
-admin.site.register(Choice)
+
+admin.site.register(Question, QuestionAdmin)
--- polls/models.py
+++ polls/models.py
@@ -1,22 +1,32 @@
-#-*- coding: utf-8 -*-
+import datetime
+
+from django.contrib import admin
 from django.db import models
-from django.urls import reverse
+from django.utils import timezone
 
 
-class Poll(models.Model):
-    question = models.CharField(max_length=200)
+class Question(models.Model):
+    objects = models.Manager()
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
 
-    def __str__(self):              # Python 3: def __unicode__(self):
-        return self.question
-    
-    def get_absolute_url(self):
-        return reverse("polls:detail", kwargs={"pk": self.pk})
+    def __str__(self):
+        return self.question_text
+
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
-    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
-    def __str__(self):              # Python 3: def __unicode__(self):
+    def __str__(self):
         return self.choice_text
